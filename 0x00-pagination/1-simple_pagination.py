aceef8b2abe11eb40cc257c1115480ea405dd345
Abimbola <abimbolamichael100@gmail.com>
2024-01-29 08:59:21 -0800

    update

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,7 +39,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
-        
+
         data = self.dataset()
         if data is None:
             return []
