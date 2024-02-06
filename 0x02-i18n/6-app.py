2519b0d81396d845b283bae5c5f77ce094e6bec5
Abimbola Michael <abimbolamichael100.com>
2024-02-06 14:15:45 +0100

    Update

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -62,7 +62,8 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
-#babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
