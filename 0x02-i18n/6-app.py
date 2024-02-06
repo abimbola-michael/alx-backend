9f99bd535d9ddf7fed09586182f1ae22198b9f9a
Abimbola Michael <abimbolamichael100.com>
2024-02-06 14:13:53 +0100

    Correction

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -47,7 +47,7 @@ def before_request():
         g.user = user
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
     """Gets locale from request"""
     locale = request.args.get("locale")
@@ -62,7 +62,7 @@ def get_locale():
         return locale
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
-babel.init_app(app, locale_selector=get_locale)
+#babel.init_app(app, locale_selector=get_locale)
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index():
